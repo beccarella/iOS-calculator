93247216e858f48ca3a192e935f0c4f6565d79ec
beccarella <urquhart.becca@gmail.com>
2019-09-26 12:28:26 +0200

    Initial Commit

---
+++ Calculator2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator2
+//
+//  Created by Rebecca Urquhart on 26/09/2019.
+//  Copyright © 2019 Rebecca Urquhart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
